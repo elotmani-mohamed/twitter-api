f3d9749b5d8006762dc458dda025b72b49e28897
elotmani-mohamed <ayur.elotmani@gmail.com>
2022-05-13 23:28:04 +0100

    api twitter first commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h1>Hello react and typescript</h1>
+      <h2>PPPPPPPPPPPPPPPPPPP</h2>
+      <p>alpha</p>
+    </div>
+  )
+}
+
+export default Home
